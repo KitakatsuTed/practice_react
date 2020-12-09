b86a0fffbfc59da5db4aeaee0ade53fb6d7c46b8
KitakatsuTed <ted@circlearound.co.jp>
2020-12-09 22:22:38 +0900

    add reducer prot

--- src/components/ui/Bank.tsx
+++ src/components/ui/Bank.tsx
@@ -1,6 +1,16 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, useReducer, useEffect} from "react";
 import {BankAccount} from "../entities";
 import Clock from "../lib/Clock";
+
+enum WithdrawStatus {
+  NOT_DOING = 'not_doing',
+  DOING = 'doing',
+  DONE = 'done'
+}
+
+interface WithdrawContext {
+  status: WithdrawStatus
+}
 
 function MoneyInput({withdrawProcess}: {withdrawProcess: (withdrawMoney: number) => boolean}) {
   const [inputMoney, setInputMoney] = useState<number>(0)
@@ -10,7 +20,8 @@ function MoneyInput({withdrawProcess}: {withdrawProcess: (withdrawMoney: number)
     setInputMoney(Number(e.target.value))
   }
 
-  const onClickTransaction = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const onClickTransaction = (e: React
+    .MouseEvent<HTMLButtonElement, MouseEvent>) => {
     if(withdrawProcess(inputMoney)) {
       return setInputMoney(0)
     }
@@ -30,6 +41,7 @@ export default function Bank({clockRef}: {clockRef: React.MutableRefObject<Clock
   const [errors, setErrors] = useState<string[]>([])
   const walletRef: React.MutableRefObject<number> = useRef<number>(1000)
   const [bankAccount, setBankAccount] = useState<BankAccount>({amount: walletRef.current})
+  const [withdrawContext, setWithdrawContext] = useState<WithdrawContext>({status: WithdrawStatus.NOT_DOING})
 
   // ここbankAccountの状態に依存しているから引き剥がしたい
   function transactionMoney(withdrawMoney: number) {
@@ -68,8 +80,50 @@ export default function Bank({clockRef}: {clockRef: React.MutableRefObject<Clock
     return '開店'
   }
 
+  const reducer = (state: WithdrawContext, action: any) => {
+    switch (action.type) {
+      case WithdrawStatus.NOT_DOING:
+        return({...state, status: WithdrawStatus.DOING})
+      case WithdrawStatus.DOING:
+        return({...state, status: WithdrawStatus.DONE})
+      case WithdrawStatus.DONE:
+        return({...state, status: WithdrawStatus.NOT_DOING})
+      default:
+        throw new Error(`unknown action type: ${action.type}`)
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, withdrawContext)
+  const startDispatch = () => {
+    return dispatch({ type: state.status })
+  }
+
+  function hoge(state: WithdrawContext) {
+    if (state.status === WithdrawStatus.NOT_DOING) {
+      return(<div>a</div>)
+    }
+    if (state.status === WithdrawStatus.DOING) {
+      return(<div>b</div>)
+    }
+    if (state.status === WithdrawStatus.DONE) {
+      return(<div>c</div>)
+    }
+  }
+
+  useEffect( () => {
+    const interval = setInterval( () => {
+      if (state.status !== WithdrawStatus.NOT_DOING) {
+        dispatch({ type: state.status })
+      }
+    }, 1000 * 5)
+
+    return () => { clearInterval(interval) }
+  }, [state])
+
   return(
     <div>
+      <button onClick={startDispatch}>aaaa</button>
+      {hoge(state)}
       <br/>
       <ul>
         {errors.map((error, index) => {
